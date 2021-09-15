84481da6c8c362a37815d2457b78aefdf1c97c2e
Mardoqueu Henrique <mardoqueu.1995@gmail.com>
2021-09-15 20:25:39 -0300

    inserindo favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
 
+
         </Head>
         <body>
           <Main />
